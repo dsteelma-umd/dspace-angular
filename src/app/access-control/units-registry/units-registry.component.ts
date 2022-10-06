0eaad9487d8e52dc40addd7876324dd4f7868c06
David P. Steelman <dsteelma@umd.edu>
2022-10-06 13:21:56 -0400

    LIBDRUM-676. Initial placeholder implementation for Units registry
    Initial placeholder implementation for the Units registry, with a
    "Units" menu item in the "Access Control" menu of the administrative
    sidebar.
    
    https://issues.umd.edu/browse/LIBDRUM-676

---
+++ src/app/access-control/units-registry/units-registry.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ds-units-registry',
+  templateUrl: './units-registry.component.html',
+  styleUrls: ['./units-registry.component.scss']
+})
+export class UnitsRegistryComponent implements OnInit {
+
+  ngOnInit(): void {
+    let foo = 'bar';
+  }
+}
