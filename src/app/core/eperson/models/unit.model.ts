10eeb0ccdeedd83c9da0136181125584a3c1fc04
David P. Steelman <dsteelma@umd.edu>
2022-10-11 08:32:46 -0400

    LIBDRUM-676. Added ability to create a new Unit
    https://issues.umd.edu/browse/LIBDRUM-676

--- src/app/core/eperson/models/unit.model.ts
+++ src/app/core/eperson/models/unit.model.ts
@@ -8,11 +8,19 @@ import { RemoteData } from 'src/app/core/data/remote-data';
 import { PaginatedList } from 'src/app/core/data/paginated-list.model';
 import { Group } from 'src/app/core/eperson/models/group.model';
 import { UNIT } from './unit.resource-type';
+import { excludeFromEquals } from '../../utilities/equals.decorators';
 
 @typedObject
 @inheritSerialization(DSpaceObject)
 export class Unit extends DSpaceObject {
   static type = UNIT;
+
+  /**
+   * A string representing the unique name of this Group
+   */
+  @excludeFromEquals
+  @autoserializeAs('name')
+  protected _name: string;
 
   @autoserialize
   facultyOnly: boolean;
