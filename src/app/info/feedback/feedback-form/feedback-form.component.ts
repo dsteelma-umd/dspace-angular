3b57acdafa0fd627d997c3765c39ed4c5e455da7
David P. Steelman <dsteelma@umd.edu>
2023-03-09 11:15:28 -0500

    WIP

--- src/app/info/feedback/feedback-form/feedback-form.component.ts
+++ src/app/info/feedback/feedback-form/feedback-form.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { take } from 'rxjs/operators';
-import { RequestService } from 'src/app/core/data/request.service';
 import { RouteService } from 'src/app/core/services/route.service';
 import { HALEndpointService } from 'src/app/core/shared/hal-endpoint.service';
 import { RemoteDataBuildService } from 'src/app/core/cache/builders/remote-data-build.service';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { merge, mergeMap, zip } from 'rxjs';
+import { zip } from 'rxjs';
 
 declare function r12gwaic1xvrtzs(): any;
 @Component({
@@ -20,7 +18,6 @@ declare function r12gwaic1xvrtzs(): any;
 export class FeedbackFormComponent implements OnInit {
 
   constructor(
-    private requestService: RequestService,
     private halService: HALEndpointService,
     protected rdb: RemoteDataBuildService,
     private httpClient: HttpClient,
@@ -37,24 +34,19 @@ export class FeedbackFormComponent implements OnInit {
   ngOnInit() {
     let linkName = 'wufoo-feedback';
     let previousUrl$ = this.routeService.getPreviousUrl();
-    // previousUrl$.subscribe((url) => { console.log(`----previous url: ${url}`) });
 
     const href$ = this.halService.getEndpoint(linkName).pipe(
       take(1),
     );
 
-    zip(previousUrl$, href$).subscribe(([previousUrl, feedbackFormUrl]) => {
-      console.log(`---previousUrl=${previousUrl}`);
-      console.log(`---url=${feedbackFormUrl}`);
-      // this.httpClient.get(results[1], { params: { 'page': 'https://example.com/abc' }, responseType: 'text' }).subscribe((data: any) => {
-      //   console.log(`---data=${data}`);
-      //   let script = data;
-      //   let myScriptElement = document.createElement('script');
-      //   myScriptElement.type = 'text/javascript';
-      //   myScriptElement.text = script;
-      //   document.body.appendChild(myScriptElement);
-      // });
+    zip(previousUrl$, href$).subscribe(([previousPage, feedbackFormUrl]) => {
+      this.httpClient.get(feedbackFormUrl, { params: { 'page': previousPage }, responseType: 'text' }).subscribe((data: any) => {
+        let script = data;
+        let myScriptElement = document.createElement('script');
+        myScriptElement.type = 'text/javascript';
+        myScriptElement.text = script;
+        document.body.appendChild(myScriptElement);
+      });
     });
-
   }
 }
