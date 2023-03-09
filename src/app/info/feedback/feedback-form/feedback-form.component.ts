e3e39a0e9a0c057af5b6ff8a42234fedd0909cc4
David P. Steelman <dsteelma@umd.edu>
2023-03-09 13:40:59 -0500

    WIP

--- src/app/info/feedback/feedback-form/feedback-form.component.ts
+++ src/app/info/feedback/feedback-form/feedback-form.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs/operators';
+import { catchError, filter, map, take, tap } from 'rxjs/operators';
 import { RouteService } from 'src/app/core/services/route.service';
 import { HALEndpointService } from 'src/app/core/shared/hal-endpoint.service';
 import { RemoteDataBuildService } from 'src/app/core/cache/builders/remote-data-build.service';
 import { HttpClient } from '@angular/common/http';
 import { zip } from 'rxjs';
+import { ConfigurationDataService } from 'src/app/core/data/configuration-data.service';
+import { RemoteData } from 'src/app/core/data/remote-data';
+import { ConfigurationProperty } from 'src/app/core/shared/configuration-property.model';
 
-declare function r12gwaic1xvrtzs(): any;
 @Component({
   selector: 'ds-feedback-form',
   templateUrl: './feedback-form.component.html',
@@ -21,22 +23,39 @@ export class FeedbackFormComponent implements OnInit {
     private halService: HALEndpointService,
     protected rdb: RemoteDataBuildService,
     private httpClient: HttpClient,
-    private routeService: RouteService
+    private routeService: RouteService,
+    private configurationService: ConfigurationDataService
   ) {
 
   }
 
   public myScriptElement: HTMLScriptElement;
+  public formHashFallback = '';
 
   /**
    * On init check if user is logged in and use its email if so
    */
   ngOnInit() {
     let linkName = 'wufoo-feedback';
+    let formHashPropertyName = 'wufoo.feedback.formHash';
     let previousUrl$ = this.routeService.getPreviousUrl();
 
     const href$ = this.halService.getEndpoint(linkName).pipe(
       take(1),
+    );
+
+    this.configurationService.findByPropertyName(formHashPropertyName).pipe(
+      filter((rd: RemoteData<ConfigurationProperty>) => rd.hasSucceeded),
+      map((remoteData: RemoteData<ConfigurationProperty>) => { return remoteData.payload; } ),
+      map((configProp: ConfigurationProperty) => { return configProp.values[0]; } ),
+      catchError(err => {
+        throw 'Error retrieving form hash';
+      })
+    ).subscribe(
+      {
+        next: (formHash) => { this.formHashFallback = formHash },
+        error: (err) => { console.log('Error retrieving form hash'); }
+      }
     );
 
     zip(previousUrl$, href$).subscribe(([previousPage, feedbackFormUrl]) => {
